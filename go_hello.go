cb9577757a49e8f6a779f09bcaeb041da968fe01
james <jamesparker0000@gmail.com>
2023-09-04 22:04:22 +0100

    test

--- go_hello.go
+++ go_hello.go
@@ -4,26 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	port := 3000
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
 		hostname, err := os.Hostname()
 		if err != nil {
-			http.Error(w, "Failed to get hostname", http.StatusInternalServerError)
+			c.String(http.StatusInternalServerError, "Failed to get hostname")
 			return
 		}
 
-		message := fmt.Sprintf("Hello from test6 %s", hostname)
+		message := fmt.Sprintf("Hello from test3 %s", hostname)
 		fmt.Println(message)
-		w.Write([]byte(message))
+		c.String(http.StatusOK, message)
 	})
 
-	fmt.Printf("Web server listening on port %d\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-	if err != nil {
-		fmt.Printf("Failed to start the server: %v\n", err)
-	}
+	router.Run(fmt.Sprintf(":%d", port))
 }
