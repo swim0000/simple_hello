ab86df3922f9bb3d4aeb7294248cca143214bcb1
james <jamesparker0000@gmail.com>
2023-09-04 20:52:46 +0100

    test

--- go/go_hello.go
+++ go/go_hello.go
@@ -16,7 +16,7 @@ func main() {
 			return
 		}
 
-		message := fmt.Sprintf("Hello from test3 %s", hostname)
+		message := fmt.Sprintf("Hello from test4 %s", hostname)
 		fmt.Println(message)
 		w.Write([]byte(message))
 	})
